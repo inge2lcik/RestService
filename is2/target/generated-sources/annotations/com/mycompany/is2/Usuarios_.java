9e840bdf56762d9de7443ee04591cc0cc5050b9c
BlackLotusPy <blacklotus.py@gmail.com>
2017-06-06 14:50:31 -0400

    is2
    06/06/2017

---
+++ is2/target/generated-sources/annotations/com/mycompany/is2/Usuarios_.java
@@ -0,0 +1,15 @@
+package com.mycompany.is2;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-06T14:43:37")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> correo;
+    public static volatile SingularAttribute<Usuarios, Integer> id;
+    public static volatile SingularAttribute<Usuarios, String> nombre;
+
+}
